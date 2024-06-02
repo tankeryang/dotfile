f135008359052dfe9854a6c93f9ddbd9bcdf1153
tankeryang <youngzyang@outlook.com>
2024-06-02 16:36:05 +0800

    feat: add alacritty link command

--- link.sh
+++ link.sh
@@ -15,3 +15,10 @@ ln -s $DOTFILE/pip $HOME/.config
 
 echo "link starship.toml ..."
 ln -s $DOTFILE/starship/starship.toml $HOME/.config
+
+echo "link alacritty.toml ..."
+mkdir -p $HOME/.config/alacritty
+if [ ! -d "$HOME/.config/alacritty/themes" ] then
+	git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
+fi
+ln -s $DOTFILE/alacritty/alacritty.toml $HOME/.config/alacritty
